4bc53cd63ce1c4cb7d5f9bab9474ebc13d9e944e
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-06-18 17:15:44 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'new post' do
-  
+
   it 'ensures that the form route works with the /new action' do
     visit new_post_path
     expect(page.status_code).to eq(200)
@@ -16,9 +16,9 @@ describe 'new post' do
     visit new_post_path
     fill_in 'post_title', with: 'My post title'
     fill_in 'post_description', with: 'My post description'
- 
+
     click_on 'Submit Post'
- 
+
     expect(page.current_path).to eq(posts_path)
     expect(page).to have_content('My post title')
     expect(page).to have_content('My post description')
